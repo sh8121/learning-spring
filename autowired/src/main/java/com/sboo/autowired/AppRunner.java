1b45b78f853897a705346b800684809a36b90878
Boo <sboo@ebay.com>
2020-06-08 17:39:09 +0900

    aaa

--- autowired/src/main/java/com/sboo/autowired/AppRunner.java
+++ autowired/src/main/java/com/sboo/autowired/AppRunner.java
@@ -1,33 +1,28 @@
 package com.sboo.autowired;
 
-
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
+
+    @Autowired
+    MessageSource messageSource;
 
     @Autowired
     ApplicationContext applicationContext;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("proto");
-        System.out.println(applicationContext.getBean(Proto.class));
-        System.out.println(applicationContext.getBean(Proto.class));
-        System.out.println(applicationContext.getBean(Proto.class));
-
-        System.out.println("single");
-        System.out.println(applicationContext.getBean(Single.class));
-        System.out.println(applicationContext.getBean(Single.class));
-        System.out.println(applicationContext.getBean(Single.class));
-
-        System.out.println("proto by single");
-        System.out.println(applicationContext.getBean(Single.class).getProto());
-        System.out.println(applicationContext.getBean(Single.class).getProto());
-        System.out.println(applicationContext.getBean(Single.class).getProto());
+        System.out.println(Locale.getDefault());
+        System.out.println(messageSource.getMessage("greeting", new String[]{"sboo"}, Locale.KOREA));
+        System.out.println(messageSource.getMessage("greeting", new String[]{"sboo"}, Locale.US));
     }
 }
--- autowired/src/main/java/com/sboo/autowired/AutowiredApplication.java
+++ autowired/src/main/java/com/sboo/autowired/AutowiredApplication.java
@@ -7,14 +7,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 import java.util.function.Supplier;
 
 @SpringBootApplication
+@PropertySource("classpath:/app.properties")
 public class AutowiredApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AutowiredApplication.class, args);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
