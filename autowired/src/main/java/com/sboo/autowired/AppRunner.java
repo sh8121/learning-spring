e7384db1f9cd099fe1057cb4af80053a9038527e
Boo <sboo@ebay.com>
2020-06-10 09:30:00 +0900

    Validation

--- autowired/src/main/java/com/sboo/autowired/AppRunner.java
+++ autowired/src/main/java/com/sboo/autowired/AppRunner.java
@@ -6,24 +6,34 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    Validator validator;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(resourceLoader.getClass());
-        Resource resource = resourceLoader.getResource("test.txt");
-        System.out.println(resource.getClass());
-
-        System.out.println(resource.exists());
-        System.out.println(resource.getDescription());
-        System.out.println(Files.readString(Path.of(resource.getURI())));
+        System.out.println(validator.getClass());
+        Event event = new Event();
+        event.setLimit(-1);
+        event.setEmail("asgasdf");
+        Errors errors = new BeanPropertyBindingResult(event, "event");
+        validator.validate(event, errors);
+        System.out.println(errors.hasErrors());
+        errors.getAllErrors().forEach(e -> {
+            System.out.println("========error code=======");
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
     }
 }
