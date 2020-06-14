0d9f09db812897583f0e78f5fd2b053f19636bf6
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-06-14 17:13:38 +0900

    dive into @SpringBootApplication

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
@@ -1,13 +1,31 @@
 package com.sboo.springboottutorial;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+
+//@ComponentScan
+//@SpringBootConfiguration
+//@EnableAutoConfiguration
+
+//@Configuration
+//@ComponentScan
 
 @SpringBootApplication
 public class SpringbootTutorialApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootTutorialApplication.class, args);
+        SpringApplication application = new SpringApplication(SpringbootTutorialApplication.class);
+//        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
+
+        //SpringApplication.run(SpringbootTutorialApplication.class, args);
     }
 
 }
