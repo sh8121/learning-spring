5e1ff0e80b1089a9cf455520f97e391559e64608
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-06-14 18:32:39 +0900

    Custom Starter & Custom AutoConfiguration

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
@@ -1,31 +1,28 @@
 package com.sboo.springboottutorial;
 
+import com.sboo.customspringbootstarter.Holoman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-
-
-//@ComponentScan
-//@SpringBootConfiguration
-//@EnableAutoConfiguration
-
-//@Configuration
-//@ComponentScan
-
 @SpringBootApplication
 public class SpringbootTutorialApplication {
-
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SpringbootTutorialApplication.class);
-//        application.setWebApplicationType(WebApplicationType.NONE);
+        application.setWebApplicationType(WebApplicationType.NONE);
         application.run(args);
-
-        //SpringApplication.run(SpringbootTutorialApplication.class, args);
     }
 
+    @Bean
+    public Holoman holoman() {
+        Holoman holoman = new Holoman();
+        holoman.setName("sbooooooo");
+        holoman.setHowLong(10);
+        return holoman;
+    }
 }
