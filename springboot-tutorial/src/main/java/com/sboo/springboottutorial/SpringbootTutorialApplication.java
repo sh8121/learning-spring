dd45b089c35f356bac1a4be44856f52712d43ff4
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-06-15 22:02:30 +0900

    Customize Servlet Container

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
@@ -1,41 +1,11 @@
 package com.sboo.springboottutorial;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringbootTutorialApplication {
-    public static void main(String[] args) throws LifecycleException {
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-        Context context =  tomcat.addContext("/", "/");
-
-        HttpServlet servlet = new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                PrintWriter writer = resp.getWriter();
-                writer.println("<html><head><title>");
-                writer.println("Tomcat");
-                writer.println("</title></head>");
-                writer.println("<body><h1>Hello Tomcat</h1></body>");
-                writer.println("</html>");
-            }
-        };
-
-        String servletName = "helloServlet";
-        tomcat.addServlet("/", servletName, servlet);
-        context.addServletMappingDecoded("/hello", servletName);
-
-        //start하고 끝남.
-        tomcat.start();
-
-        //요청을 기다림
-        tomcat.getServer().await();
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTutorialApplication.class, args);
     }
 }
