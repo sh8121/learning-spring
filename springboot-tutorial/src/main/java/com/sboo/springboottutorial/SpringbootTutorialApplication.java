1aef6a187395a5a6992fc729f9ee867a5129a671
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-06-16 21:19:19 +0900

    independent excutable jar

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringbootTutorialApplication {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
         return "Hello Spring";
     }
