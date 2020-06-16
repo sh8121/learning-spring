7099c043028c4c742b87cdbfa76eab1f1e0681d8
Boo <sboo@ebay.com>
2020-06-16 15:10:42 +0900

    Tomcat Http2

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
@@ -22,16 +22,4 @@ public class SpringbootTutorialApplication {
         SpringApplication.run(SpringbootTutorialApplication.class, args);
     }
 
-    @Bean
-    public ServletWebServerFactory serverFactory() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(8080);
-        return connector;
-    }
 }
