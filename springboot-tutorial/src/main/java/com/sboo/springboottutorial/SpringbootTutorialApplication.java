432bb62c94e1262b287918929aa6696ca383f243
Boo <sboo@ebay.com>
2020-06-17 08:24:17 +0900

    banner

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
@@ -2,20 +2,13 @@ package com.sboo.springboottutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringbootTutorialApplication {
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello Spring";
-    }
-
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootTutorialApplication.class, args);
+        //SpringApplication.run(SpringbootTutorialApplication.class, args);
+        SpringApplication app = new SpringApplication(SpringbootTutorialApplication.class);
+        app.run(args);
     }
 
 }
