3f671c5e137596c6bbcb37af0368cff47e3ea7c3
Boo <sboo@ebay.com>
2020-06-17 08:39:13 +0900

    SpringApplicationBuilder

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
@@ -3,6 +3,7 @@ package com.sboo.springboottutorial;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.env.Environment;
 
 import java.io.PrintStream;
@@ -10,19 +11,22 @@ import java.io.PrintStream;
 @SpringBootApplication
 public class SpringbootTutorialApplication {
     public static void main(String[] args) {
-        //SpringApplication.run(SpringbootTutorialApplication.class, args);
-        SpringApplication app = new SpringApplication(SpringbootTutorialApplication.class);
-        app.setBanner(new Banner() {
-            @Override
-            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
-                out.println("=========================");
-                out.println("=========================");
-                out.println("=========================");
-                out.println("=========================");
-            }
-        });
-        //app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
+//        SpringApplication app = new SpringApplicationBuilder()
+//                .sources(SpringbootTutorialApplication.class)
+//                .banner((environment, sourceClass, out) -> {
+//                    out.println("=======================");
+//                    out.println("=======================");
+//                })
+//                .build(args);
+//        app.run();
+
+        new SpringApplicationBuilder()
+                .sources(SpringbootTutorialApplication.class)
+                .banner((environment, sourceClass, out) -> {
+                    out.println("=======================");
+                    out.println("=======================");
+                })
+                .run(args);
     }
 
 }
