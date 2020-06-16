3dd55fec82fa81eb7f2f118641392927f3337f0e
Boo <sboo@ebay.com>
2020-06-17 08:31:28 +0900

    change banner file path
    set banner in code

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
@@ -1,13 +1,27 @@
 package com.sboo.springboottutorial;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class SpringbootTutorialApplication {
     public static void main(String[] args) {
         //SpringApplication.run(SpringbootTutorialApplication.class, args);
         SpringApplication app = new SpringApplication(SpringbootTutorialApplication.class);
+        app.setBanner(new Banner() {
+            @Override
+            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+                out.println("=========================");
+                out.println("=========================");
+                out.println("=========================");
+                out.println("=========================");
+            }
+        });
+        //app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
     }
 
