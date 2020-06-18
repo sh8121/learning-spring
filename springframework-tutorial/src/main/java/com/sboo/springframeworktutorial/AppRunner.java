cb8a1e4af866f2b1528394802c9b27720544cbf1
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-06-18 21:25:33 +0900

    Initialization Phase BeanPostProcessor

--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AppRunner.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AppRunner.java
@@ -1,17 +1,22 @@
 package com.sboo.springframeworktutorial;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
+
     @Autowired
-    BookService bookService;
+    ApplicationContext applicationContext;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        bookService.printBookRepository();
+        AutowiredAnnotationBeanPostProcessor beanPostProcessor
+                = applicationContext.getBean(AutowiredAnnotationBeanPostProcessor.class);
+        System.out.println(beanPostProcessor);
     }
 }
--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/BookService.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/BookService.java
@@ -1,18 +1,28 @@
 package com.sboo.springframeworktutorial;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
-public class BookService {
+public class BookService implements InitializingBean {
 
     @Autowired
     BookRepository myBookRepository;
 
-    public void printBookRepository() {
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("postConstruct");
+        System.out.println(myBookRepository.getClass());
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
         System.out.println(myBookRepository.getClass());
     }
 }
