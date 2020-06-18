89f2acb9e689ff23cbde0dbdb5629559d529498d
Boo <sboo@ebay.com>
2020-06-18 15:30:45 +0900

    @Validated + JSR303

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SbooProperties.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SbooProperties.java
@@ -3,17 +3,23 @@ package com.sboo.springboottutorial;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.convert.DurationUnit;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 @Component
 @ConfigurationProperties("sboo")
+@Validated
 public class SbooProperties {
+    @NotEmpty
     private String name;
+
     private int age;
     private String fullName;
-    
+
     private Duration sessionTimeout = Duration.ofSeconds(30);
 
     public String getName() {
