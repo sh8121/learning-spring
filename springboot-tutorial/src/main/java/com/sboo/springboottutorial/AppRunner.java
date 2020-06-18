51db4cafb07665e55ffe72619ade91eaf81ba090
Boo <sboo@ebay.com>
2020-06-18 13:15:02 +0900

    @ConfigurationProperties as @EnableConfigurationProperties or @Component

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/AppRunner.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/AppRunner.java
@@ -1,5 +1,6 @@
 package com.sboo.springboottutorial;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -9,21 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Value("${sboo.name}")
-    private String name;
-
-    @Value("${sboo.fullName}")
-    private String fullName;
-
-    @Value("${sboo.age}")
-    private int age;
+    @Autowired
+    SbooProperties sbooProperties;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("===================");
-        System.out.println(name);
-        System.out.println(fullName);
-        System.out.println(age);
+        System.out.println(sbooProperties.getName());
+        System.out.println(sbooProperties.getFullName());
+        System.out.println(sbooProperties.getAge());
         System.out.println("===================");
     }
 }
--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SpringbootTutorialApplication.java
@@ -3,9 +3,13 @@ package com.sboo.springboottutorial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+//@EnableConfigurationProperties(SbooProperties.class)
 public class SpringbootTutorialApplication {
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SpringbootTutorialApplication.class);
         app.setWebApplicationType(WebApplicationType.NONE);
