44c307651b8559f92025fecbba64a56a9ec3abd9
Boo <sboo@ebay.com>
2020-06-19 17:30:27 +0900

    Slice Test(@WebMvcTest)

--- springboot-tutorial/src/test/java/com/sboo/springboottutorial/sample/SampleControllerTest.java
+++ springboot-tutorial/src/test/java/com/sboo/springboottutorial/sample/SampleControllerTest.java
@@ -3,30 +3,35 @@ package com.sboo.springboottutorial.sample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
-    @Autowired
-    WebTestClient webTestClient;
+    //TODO 다른 슬라이스 테스트
 
     @MockBean
     SampleService mockSampleService;
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
     public void hello() throws Exception {
         when(mockSampleService.getName()).thenReturn("SangHoon");
-        webTestClient.get().uri("/hello").exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("helloSangHoon");
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string("helloSangHoon"));
     }
 }
