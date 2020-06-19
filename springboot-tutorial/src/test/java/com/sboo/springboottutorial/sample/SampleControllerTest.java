848879f67d7f6d9e8971906ac9529da0e3b5ed35
Boo <sboo@ebay.com>
2020-06-19 15:06:00 +0900

    with WebTestClient

--- springboot-tutorial/src/test/java/com/sboo/springboottutorial/sample/SampleControllerTest.java
+++ springboot-tutorial/src/test/java/com/sboo/springboottutorial/sample/SampleControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -16,7 +17,7 @@ import static org.mockito.Mockito.when;
 public class SampleControllerTest {
 
     @Autowired
-    TestRestTemplate testRestTemplate;
+    WebTestClient webTestClient;
 
     @MockBean
     SampleService mockSampleService;
@@ -24,7 +25,8 @@ public class SampleControllerTest {
     @Test
     public void hello() throws Exception {
         when(mockSampleService.getName()).thenReturn("SangHoon");
-        String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("helloSangHoon");
+        webTestClient.get().uri("/hello").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("helloSangHoon");
     }
 }
