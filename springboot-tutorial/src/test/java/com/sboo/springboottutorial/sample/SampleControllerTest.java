40f464c9f344226fe756a7f0c51fdd3e6996c744
Boo <sboo@ebay.com>
2020-06-19 14:46:07 +0900

    Bean Mocking(Divide Test Layer)

--- springboot-tutorial/src/test/java/com/sboo/springboottutorial/sample/SampleControllerTest.java
+++ springboot-tutorial/src/test/java/com/sboo/springboottutorial/sample/SampleControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,9 +18,13 @@ public class SampleControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() throws Exception {
+        when(mockSampleService.getName()).thenReturn("SangHoon");
         String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hellosboo");
+        assertThat(result).isEqualTo("helloSangHoon");
     }
 }
