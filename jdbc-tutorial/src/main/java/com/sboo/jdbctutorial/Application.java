90ecb54ecefddabfd3da574b0022d63aa36992ec
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-06-20 22:35:03 +0900

    jdbc intro

---
+++ jdbc-tutorial/src/main/java/com/sboo/jdbctutorial/Application.java
@@ -0,0 +1,23 @@
+package com.sboo.jdbctutorial;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Application {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/spring";
+        String username = "admin";
+        String password = "admin";
+
+        try(Connection connection = DriverManager.getConnection(url, username, password)){
+            System.out.println("Connection Created: " + connection);
+            //String sql = "CREATE TABLE ACCOUNT (id int, username varchar(255), password varchar(255));";
+            String sql = "INSERT INTO ACCOUNT VALUES(1, 'sboo', 'sboo');";
+            try(PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.execute();
+            }
+        }
+    }
+}
