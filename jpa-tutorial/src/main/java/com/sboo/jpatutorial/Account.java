78d2b1b367b99786751f0eab89c0d270acf61e74
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-06-21 16:47:33 +0900

    ddl-auto=update & domain class update(field 삭제는 반영 안됨 주의!)

--- jpa-tutorial/src/main/java/com/sboo/jpatutorial/Account.java
+++ jpa-tutorial/src/main/java/com/sboo/jpatutorial/Account.java
@@ -16,6 +16,9 @@ public class Account {
 //    @Column
     private String password;
 
+//    @Column
+    private String email;
+
     public Long getId() {
         return id;
     }
@@ -38,5 +41,13 @@ public class Account {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
