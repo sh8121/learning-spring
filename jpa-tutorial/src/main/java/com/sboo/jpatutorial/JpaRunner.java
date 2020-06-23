d6bebec5a3e3887c869a13505cfc286c7c031645
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-06-23 21:21:15 +0900

    Criteria (Type Safe Query)

--- jpa-tutorial/src/main/java/com/sboo/jpatutorial/JpaRunner.java
+++ jpa-tutorial/src/main/java/com/sboo/jpatutorial/JpaRunner.java
@@ -11,6 +11,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Component
@@ -23,9 +26,11 @@ public class JpaRunner implements ApplicationRunner {
 //    @Transactional
     @Override
     public void run(ApplicationArguments args) throws Exception {
-//        Query query = entityManager.createQuery("SELECT p FROM Post AS p");
-        TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post AS p", Post.class);
-        List<Post> posts = query.getResultList();
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Post> query = builder.createQuery(Post.class);
+        Root<Post> root = query.from(Post.class);
+        query.select(root);
+        List<Post> posts = entityManager.createQuery(query).getResultList();
         posts.forEach(System.out::println);
     }
 }
