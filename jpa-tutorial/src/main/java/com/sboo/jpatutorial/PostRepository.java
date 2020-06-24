95bf4f5897793e1ee012fcd6846ddb385dcf13fd
Boo <sboo@ebay.com>
2020-06-24 10:29:23 +0900

    Manual Repository

---
+++ jpa-tutorial/src/main/java/com/sboo/jpatutorial/PostRepository.java
@@ -0,0 +1,30 @@
+package com.sboo.jpatutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@Transactional
+public class PostRepository {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Post add(Post post) {
+        entityManager.persist(post);
+        return post;
+    }
+
+    public void delete(Post post) {
+        entityManager.remove(post);
+    }
+
+    public List<Post> findAll() {
+        return entityManager.createQuery("SELECT p FROM Post AS p", Post.class)
+                .getResultList();
+    }
+}
