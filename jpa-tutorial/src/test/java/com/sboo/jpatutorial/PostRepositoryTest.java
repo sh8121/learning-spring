e255abf234bccc87ba7a6c2805a24024183f1a7e
Boo <sboo@ebay.com>
2020-06-24 14:36:21 +0900

    Rollback false in Test
    CrudRepository
    PagingAndSortingRepository

--- jpa-tutorial/src/test/java/com/sboo/jpatutorial/PostRepositoryTest.java
+++ jpa-tutorial/src/test/java/com/sboo/jpatutorial/PostRepositoryTest.java
@@ -4,7 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +22,7 @@ public class PostRepositoryTest {
     PostRepository postRepository;
 
     @Test
+    @Rollback(false)
     public void crudRepository() {
         //Given
         Post post = new Post();
@@ -27,5 +34,24 @@ public class PostRepositoryTest {
 
         //Then
         assertThat(newPost.getId()).isNotNull();
+
+        //When
+        //findAll과 Select Query 발생 비교.
+        Optional<Post> thePost = postRepository.findById(newPost.getId());
+        // When
+        List<Post> posts = postRepository.findAll();
+
+        //Then
+        assertThat(posts.size()).isEqualTo(1);
+        assertThat(posts).contains(newPost);
+
+        //When
+        Page<Post> page = postRepository.findAll(PageRequest.of(0, 10));
+
+        //Then
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
     }
 }
