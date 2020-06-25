7c031bc534c372a63b617d1139e4fefa5ae087d1
Boo <sboo@ebay.com>
2020-06-25 14:02:33 +0900

    QueryLookupStrategy

--- jpa-tutorial/src/main/java/com/sboo/jpatutorial/CommentRepository.java
+++ jpa-tutorial/src/main/java/com/sboo/jpatutorial/CommentRepository.java
@@ -1,4 +1,12 @@
 package com.sboo.jpatutorial;
 
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 public interface CommentRepository extends MyRepository<Comment, Long> {
+
+//    @Query(value = "SELECT c FROM Comment AS c ", nativeQuery = true)
+    @Query("SELECT c FROM Comment AS c ")
+    List<Comment> findByTitleContains(String  keyword);
 }
--- jpa-tutorial/src/main/java/com/sboo/jpatutorial/JpaTutorialApplication.java
+++ jpa-tutorial/src/main/java/com/sboo/jpatutorial/JpaTutorialApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.QueryLookupStrategy;
 
 @SpringBootApplication
-//@EnableJpaRepositories
+@EnableJpaRepositories(queryLookupStrategy = QueryLookupStrategy.Key.CREATE_IF_NOT_FOUND)
 @Import(SbooRegistrar.class)
 public class JpaTutorialApplication {
 
