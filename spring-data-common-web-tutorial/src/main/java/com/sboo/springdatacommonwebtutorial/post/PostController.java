f364d0c43d0d40ca85c85aa769598d716ba1844a
Boo <sboo@ebay.com>
2020-06-29 13:53:33 +0900

    Pageable, Sort

--- spring-data-common-web-tutorial/src/main/java/com/sboo/springdatacommonwebtutorial/post/PostController.java
+++ spring-data-common-web-tutorial/src/main/java/com/sboo/springdatacommonwebtutorial/post/PostController.java
@@ -1,6 +1,8 @@
 package com.sboo.springdatacommonwebtutorial.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +23,8 @@ public class PostController {
         return post.getTitle();
     }
 
+    @GetMapping("/posts")
+    public Page<Post> getPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
 }
--- spring-data-common-web-tutorial/src/test/java/com/sboo/springdatacommonwebtutorial/post/PostControllerTest.java
+++ spring-data-common-web-tutorial/src/test/java/com/sboo/springdatacommonwebtutorial/post/PostControllerTest.java
@@ -6,12 +6,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,5 +35,21 @@ class PostControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("jpa"));
 
+    }
+
+    @Test
+    public void getPosts() throws Exception {
+        Post post = new Post();
+        post.setTitle("jpa");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts")
+                    .param("page", "0")
+                    .param("size", "10")
+                    .param("sort", "created,desc")
+                    .param("sort", "title"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].title", is("jpa")));
     }
 }
