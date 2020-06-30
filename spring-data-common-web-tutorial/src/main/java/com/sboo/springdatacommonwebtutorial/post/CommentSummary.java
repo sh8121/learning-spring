765e042e3ddcba9c21b603f3a0d8b21f59eaa219
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-06-30 20:46:10 +0900

    Open Projection

--- spring-data-common-web-tutorial/src/main/java/com/sboo/springdatacommonwebtutorial/post/CommentSummary.java
+++ spring-data-common-web-tutorial/src/main/java/com/sboo/springdatacommonwebtutorial/post/CommentSummary.java
@@ -1,7 +1,13 @@
 package com.sboo.springdatacommonwebtutorial.post;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface CommentSummary {
     String getComment();
     int getUp();
     int getDown();
+
+    //Open Projection
+    @Value("#{target.up + ' ' + target.down}")
+    String getVotes();
 }
--- spring-data-common-web-tutorial/src/test/java/com/sboo/springdatacommonwebtutorial/post/CommentRepositoryTest.java
+++ spring-data-common-web-tutorial/src/test/java/com/sboo/springdatacommonwebtutorial/post/CommentRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import javax.persistence.NamedAttributeNode;
 import javax.persistence.NamedEntityGraph;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,8 +17,25 @@ public class CommentRepositoryTest {
     @Autowired
     CommentRepository commentRepository;
 
+    @Autowired
+    PostRepository postRepository;
+
     @Test
     public void getComment() {
-        commentRepository.findByPost_Id(1l);
+        Post post = new Post();
+        post.setTitle("jpa");
+        Post savedPost = postRepository.save(post);
+
+        Comment comment = new Comment();
+        comment.setPost(savedPost);
+        comment.setUp(10);
+        comment.setDown(1);
+        commentRepository.save(comment);
+
+        commentRepository.findByPost_Id(savedPost.getId())
+                .forEach(c -> {
+                    System.out.println("================");
+                    System.out.println(c.getVotes());
+                });
     }
 }
