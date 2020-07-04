8eef5e3c923b715b03edfac043b646d8526c6aa6
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-07-04 18:16:07 +0900

    Spring Boot Web MVC Intro

---
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.sboo.springboottutorial.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//@EnableWebMvc : Spring Boot 제공 MVC 기능 모두 사라짐.
+public class WebConfig implements WebMvcConfigurer {
+
+}
