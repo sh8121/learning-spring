9837739d5372f1c497fb1c6d95be8b47f5789f47
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-07-05 20:04:23 +0900

    HttpMessageConverters

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/user/UserController.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/user/UserController.java
@@ -1,12 +1,21 @@
 package com.sboo.springboottutorial.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+//    @PostMapping("/user")
+//    public /* @ResponseBody */ User create(@RequestBody User user) {
+//        return null;
+//    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user) {
+        return user;
     }
 }
