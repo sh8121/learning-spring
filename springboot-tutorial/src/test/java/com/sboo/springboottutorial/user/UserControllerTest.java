e7776646a40d8cb97bfe3e607da9109e6030d271
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-07-05 20:37:10 +0900

    ContentNegotiationVIewResolver?

--- springboot-tutorial/src/test/java/com/sboo/springboottutorial/user/UserControllerTest.java
+++ springboot-tutorial/src/test/java/com/sboo/springboottutorial/user/UserControllerTest.java
@@ -28,14 +28,16 @@ public class UserControllerTest {
     }
 
     @Test
-    public void createUser_JSON() throws Exception {
+    public void createUser_XML() throws Exception {
         String userJson = "{\"username\":\"sboo\", \"password\": \"123\"}";
         mockMvc.perform(post("/users/create")
         .contentType(MediaType.APPLICATION_JSON)
-        .accept(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_XML)
         .content(userJson))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.username", is(equalTo("sboo"))))
-                .andExpect(jsonPath("$.password", is(equalTo("123"))));
+                .andExpect(xpath("/User/username")
+                        .string("sboo"))
+                .andExpect(xpath("/User/password")
+                        .string("123"));
     }
 }
