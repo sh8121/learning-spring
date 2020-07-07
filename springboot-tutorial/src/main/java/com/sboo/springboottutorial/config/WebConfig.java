043997e94e367d4eeb835905c542b3c7c50b6340
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-07-07 21:39:27 +0900

    Static Resource

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/config/WebConfig.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/config/WebConfig.java
@@ -2,10 +2,16 @@ package com.sboo.springboottutorial.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //@EnableWebMvc : Spring Boot 제공 MVC 기능 모두 사라짐.
 public class WebConfig implements WebMvcConfigurer {
-
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/")
+                .setCachePeriod(20);
+    }
 }
