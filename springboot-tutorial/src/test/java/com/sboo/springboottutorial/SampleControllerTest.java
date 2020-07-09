4d4c8e708dc6699b867f81e0fba22e0196f89169
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-07-09 21:48:09 +0900

    Thymeleaf Draft

---
+++ springboot-tutorial/src/test/java/com/sboo/springboottutorial/SampleControllerTest.java
@@ -0,0 +1,27 @@
+package com.sboo.springboottutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(SampleController.class)
+class SampleControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("sboo")));
+    }
+
+}
