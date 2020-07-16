a0e3230c50da9fd6380c4a4d291ac3012bb887dd
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-07-16 21:55:29 +0900

    ExceptionHandler(TODO: ControllerAdvice)

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/SampleController.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SampleController.java
@@ -1,15 +1,33 @@
 package com.sboo.springboottutorial;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
+
+    @GetMapping("/exception")
+    public void exception() {
+        throw new SampleException();
+    }
+
     @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("name", "sboo");
         return "hello";
+    }
+
+    @ExceptionHandler(SampleException.class)
+    @ResponseBody
+    public AppError sampleError(SampleException exception) {
+        AppError appError = new AppError();
+        appError.setMessage("error.app.key");
+        appError.setReason("akakakakakak");
+        return appError;
     }
 
 }
---
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/SampleException.java
@@ -0,0 +1,4 @@
+package com.sboo.springboottutorial;
+
+public class SampleException extends RuntimeException {
+}
