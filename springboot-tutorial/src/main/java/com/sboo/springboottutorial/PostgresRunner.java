48edd6b82356a0f5b351f5772668cb09d4291f30
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-07-21 21:35:47 +0900

    Postgresql

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/PostgresRunner.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/PostgresRunner.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 @Component
-public class MysqlRunner implements ApplicationRunner {
+public class PostgresRunner implements ApplicationRunner {
 
     @Autowired
     DataSource dataSource;
@@ -23,13 +23,14 @@ public class MysqlRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         try(Connection connection = dataSource.getConnection()) {
             System.out.println(dataSource.getClass()); //DBCP
+            System.out.println(connection.getMetaData().getDriverName());
             System.out.println(connection.getMetaData().getURL());
             System.out.println(connection.getMetaData().getUserName());
             Statement statement = connection.createStatement();
-            String sql = "CREATE TABLE USER(id INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY(id))";
+            String sql = "CREATE TABLE ACCOUNT(id INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY(id))";
             statement.executeUpdate(sql);
         }
 
-        jdbcTemplate.execute("INSERT INTO USER VALUES(1, 'sboo')");
+        jdbcTemplate.execute("INSERT INTO ACCOUNT VALUES(1, 'sboo')");
     }
 }
