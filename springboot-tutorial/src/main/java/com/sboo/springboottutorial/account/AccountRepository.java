99eb59438203533e4448b2a83ba2d52e24652e77
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-07-26 17:51:38 +0900

    RestClient-RestTemplate

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/account/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sboo.springboottutorial.account;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccountRepository extends CrudRepository<Account, String> {
-}
