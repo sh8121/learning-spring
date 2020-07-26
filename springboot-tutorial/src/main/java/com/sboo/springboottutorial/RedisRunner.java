69835079ffe76f60184a2ce6a6c1e580098a2252
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-07-26 15:15:48 +0900

    redis with springboot basic

---
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/RedisRunner.java
@@ -0,0 +1,23 @@
+package com.sboo.springboottutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisRunner implements ApplicationRunner {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ValueOperations<String, String> valueOps = redisTemplate.opsForValue();
+        valueOps.set("sboo", "Sang hoon");
+        valueOps.set("springboot", "2.0");
+        valueOps.set("hello", "world");
+    }
+}
