b565e42e52e152081a70aade85eec42a32049b7e
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-07-26 14:52:55 +0900

    schema update with Flyway

--- springboot-tutorial/src/main/java/com/sboo/springboottutorial/account/Account.java
+++ springboot-tutorial/src/main/java/com/sboo/springboottutorial/account/Account.java
@@ -15,6 +15,10 @@ public class Account {
 
     private String password;
 
+    private String email;
+
+    private boolean active;
+
     public Long getId() {
         return id;
     }
@@ -37,6 +41,22 @@ public class Account {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
     @Override
