00bf82ef8bb3e32adc592ede4de2b86344add1f5
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-26 19:49:48 +0900

    Bean Scope - Scoped Proxy

--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/Proto.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/Proto.java
@@ -2,10 +2,12 @@ package com.sboo.springframeworktutorial;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+//Proxy Bean을 만들어서 주입.
 public class Proto {
 //    @Autowired
 //    Single single;
