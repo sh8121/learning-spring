57d8916a87abb8baa52a939a00a2013d1f2effa9
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-26 19:32:10 +0900

    Bean Scope - Prototype vs Singleton

--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AppRunner.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AppRunner.java
@@ -3,20 +3,25 @@ package com.sboo.springframeworktutorial;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    Single single;
-
-    @Autowired
-    Proto proto;
+    ApplicationContext context;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(proto);
-        System.out.println(single.getProto());
+        System.out.println("proto");
+        System.out.println(context.getBean(Proto.class));
+        System.out.println(context.getBean(Proto.class));
+        System.out.println(context.getBean(Proto.class));
+
+        System.out.println("single");
+        System.out.println(context.getBean(Single.class));
+        System.out.println(context.getBean(Single.class));
+        System.out.println(context.getBean(Single.class));
     }
 }
