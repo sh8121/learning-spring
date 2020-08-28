846fe650d7ec490d80150ac453d51f1f30f9d2d5
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-28 18:55:21 +0900

    Environment - Profile for ComponentScan

--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/TestBookRepository.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/TestBookRepository.java
@@ -1,5 +1,11 @@
 package com.sboo.springframeworktutorial;
 
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("test")
 public class TestBookRepository implements BookRepository {
 }
