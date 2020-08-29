11c6b317962498fd5758c2a75bfa2ceaf1e52d62
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-29 14:27:15 +0900

    MessageSource - Spring Boot Default

--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AppRunner.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AppRunner.java
@@ -1,28 +1,24 @@
 package com.sboo.springframeworktutorial;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationContext context;
-
-    @Value("${app.name}")
-    private String appName;
+    private MessageSource messageSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Environment environment = context.getEnvironment();
-        System.out.println(environment.getProperty("app.name"));
-        System.out.println(appName);
+        System.out.println(messageSource.getClass());
+        System.out.println(messageSource.getMessage("greeting", new String[]{"Sang Hoon"}, Locale.getDefault()));
+        System.out.println(messageSource.getMessage("greeting", new String[]{"Sang Hoon"}, Locale.KOREA));
     }
 }
--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/SpringframeworkTutorialApplication.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/SpringframeworkTutorialApplication.java
@@ -2,10 +2,12 @@ package com.sboo.springframeworktutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
-@PropertySource("classpath:/app.properties")
 public class SpringframeworkTutorialApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringframeworkTutorialApplication.class, args);
