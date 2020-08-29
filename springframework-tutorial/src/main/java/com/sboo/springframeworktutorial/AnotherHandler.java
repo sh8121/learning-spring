1a972a11048a6a4452434e5507be09b738ff650d
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-29 15:29:05 +0900

    ApplicationEventPublisher - Fixed Order

--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AnotherHandler.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AnotherHandler.java
@@ -1,11 +1,15 @@
 package com.sboo.springframeworktutorial;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class AnotherHandler {
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE + 2)
     public void handle(MyEvent myEvent) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("Another " + myEvent.getData());
--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/MyEventHandler.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/MyEventHandler.java
@@ -2,12 +2,15 @@ package com.sboo.springframeworktutorial;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyEventHandler {
 
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public void handle(MyEvent myEvent) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트 받았다. 데이터는 " + myEvent.getData());
