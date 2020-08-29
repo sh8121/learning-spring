67049b3a7017c6fc90925c75bceb8d740002444e
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-29 15:33:37 +0900

    ApplicationEventPublisher - Async

--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/SpringframeworkTutorialApplication.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/SpringframeworkTutorialApplication.java
@@ -2,8 +2,10 @@ package com.sboo.springframeworktutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringframeworkTutorialApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringframeworkTutorialApplication.class, args);
