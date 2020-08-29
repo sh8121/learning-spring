15fae0ae110f3c49b2a26abbcee2eefd221fe7c1
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-29 18:12:36 +0900

    ResourceLoader

--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AppRunner.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AppRunner.java
@@ -3,17 +3,28 @@ package com.sboo.springframeworktutorial;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationEventPublisher applicationEventPublisher;
+    private ResourceLoader resourceLoader;
+
+//    @Autowired
+//    private ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        applicationEventPublisher.publishEvent(new MyEvent(this, 100));
+        Resource resource = resourceLoader.getResource("classpath:/test.txt");
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
+        System.out.println(Files.readString(Path.of(resource.getURI())));
     }
 }
