3a3e00dcdab33dd0c4d0cb3005240994355560be
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-29 15:20:56 +0900

    ApplicationEventPublisher - Multi Listener -> default sequential(but don't know the exact order)

--- springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AnotherHandler.java
+++ springframework-tutorial/src/main/java/com/sboo/springframeworktutorial/AnotherHandler.java
@@ -1,15 +1,13 @@
 package com.sboo.springframeworktutorial;
 
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyEventHandler {
-
+public class AnotherHandler {
     @EventListener
     public void handle(MyEvent myEvent) {
         System.out.println(Thread.currentThread().toString());
-        System.out.println("이벤트 받았다. 데이터는 " + myEvent.getData());
+        System.out.println("Another " + myEvent.getData());
     }
 }
